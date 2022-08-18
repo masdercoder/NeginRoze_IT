ae750ab39404b0d675f56e5c0155c97faa8c1c56
masdercoder <masderfach@gmail.com>
2022-08-18 10:04:02 +0430

    new model

---
+++ lib/page/profile/controller/fd.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MAsder extends StatefulWidget {
+  const MAsder({super.key});
+
+  @override
+  State<MAsder> createState() => _MAsderState();
+}
+
+class _MAsderState extends State<MAsder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
